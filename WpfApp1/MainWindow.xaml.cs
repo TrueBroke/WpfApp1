42949df74b7f75679471d64675b507e1849fe0a1
broke903 <143308560+broke903@users.noreply.github.com>
2024-08-03 19:44:16 +1000

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+
+        }
+
+        private void SetUpGame()
+        {
+            List<string> AnimalEmoji = new List<string>()
+            {
+                "🐷","🐷",
+                "🐶","🐶",
+                "💩","💩",
+                "🦒","🦒",
+                "👻","👻",
+                "🐔","🐔",
+                "🐸"," 🐸",
+                "🦄","🦄",
+            };
+            
+            Random rnd = new Random();
+
+            foreach (TextBlock TextBlock in MainGrid.Children.OfType<TextBlock>())
+            {
+                int index = rnd.Next(AnimalEmoji.Count);
+                string NextEmoji = AnimalEmoji[index];
+                TextBlock.Text = NextEmoji;
+                AnimalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
